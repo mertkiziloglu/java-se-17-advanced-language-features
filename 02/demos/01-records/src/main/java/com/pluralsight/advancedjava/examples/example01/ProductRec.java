876d97d776a73e0f453cdc276cc16ec8395f566f
mertkiziloglu <mert_besiktas24@hotmail.com>
2023-05-25 22:25:57 +0300

    Exercise01Test

--- 02/demos/01-records/src/main/java/com/pluralsight/advancedjava/examples/example01/ProductRec.java
+++ 02/demos/01-records/src/main/java/com/pluralsight/advancedjava/examples/example01/ProductRec.java
@@ -1,8 +1,4 @@
-/*
- * This code is part of the course "Java SE 17 Advanced Language Features" for Pluralsight.
- *
- * Copyright (C) 2022 by Jesper de Jong (jesper@jdj-it.com).
- */
+
 package com.pluralsight.advancedjava.examples.example01;
 
 /**
--- 02/demos/01-records/src/main/java/com/pluralsight/advancedjava/exercises/exercise01/Customer.java
+++ 02/demos/01-records/src/main/java/com/pluralsight/advancedjava/exercises/exercise01/Customer.java
@@ -1,8 +1,4 @@
-/*
- * This code is part of the course "Java SE 17 Advanced Language Features" for Pluralsight.
- *
- * Copyright (C) 2022 by Jesper de Jong (jesper@jdj-it.com).
- */
+
 package com.pluralsight.advancedjava.exercises.exercise01;
 
 /**
@@ -12,33 +8,30 @@ package com.pluralsight.advancedjava.exercises.exercise01;
  *       Don't make it a record for this exercise - the point here is to learn how to make a regular class immutable.
  *       Run the included unit test {@link Exercise01Test} to check if you have done this correctly.
  */
-public class Customer {
+public final class Customer {
 
-    private int id;
-    private String name;
-    private String email;
+    private final int id;
+    private final String name;
+    private final String email;
+
+    public Customer(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
